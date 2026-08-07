9f6079cb6bf03cfcbf90014df6087b22f1ab83eb
RivicsonWaikhom <rivicson2004@gmail.com>
2026-08-08 00:08:56 +0530

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public int longestConsecutive(int[] nums) {
+        Map<Integer,Boolean> travel= new HashMap<>();
+        for (int i : nums) {
+            travel.put(i,Boolean.FALSE);
+        }
+        int longest = 0;
+        for( int i: nums){
+            int currLength=1;
+            // ->check
+            int next=i+1;
+            while(travel.containsKey(next)&& (travel.get(next)==false)){
+                currLength++;
+                travel.put(i,Boolean.TRUE);
+                next++;
+            }
+
+            // check <--
+            int prev=i-1;
+            while(travel.containsKey(prev)&& (travel.get(prev)==false)){
+                currLength++;
+                travel.put(prev,Boolean.TRUE);
+                prev--;
+            }
+            longest= Math.max(currLength,longest);
+
+        }
+
+         
+        return longest;
+    }
+}
